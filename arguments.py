9c38f51cfb012183d1ac66a89a3e5de96e6efc0f
Windwalker1314 <732773140@qq.com>
2023-03-03 10:44:30 +0800

    test

--- arguments.py
+++ arguments.py
@@ -5,7 +5,7 @@ def get_common_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--base_model_path", type=str, default="D:\\Data\\conformer\\hubert-large\chinese-hubert-large", help="The hubert model path")
     parser.add_argument("--model_path", type=str, default="./model", help="Model path")
-    parser.add_argument("--model_name", type=str, default="LSTM_aug", help="Model Name")
+    parser.add_argument("--model_name", type=str, default="LSTM_v2.0", help="Model Name")
     parser.add_argument("--sampling_rate", type=int, default=16000, help="Sampling rate of wav file (resample)")
     parser.add_argument("--audio_section_length", type=float, default=1, help="max_audio_length")
     args = parser.parse_args()
